94e629cb169ef144f2be33b5444ff953a77befc3
Samuel <omilosamuel@gmail.com>
2022-08-06 18:47:33 +0100

    tests: add test for users

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,5 +1,36 @@
+import express from "express"
+import request from "supertest"
+import { init } from "../src/loaders"
+
 describe('User tests', () => {
-    test('Create user', () => {
-        expect(1 + 1).toBe(2)
+    const app = express()
+    beforeAll(async () => {
+        await init({ expressApp: app })
     })
+    test('Create user', async () => {
+        const res = await request(app).post('/api/users').send({
+            email: "sampleuser@gmail.com",
+            firstName: "Samuel",
+            lastName: "Sample",
+            password: "password"
+        })
+
+        expect(res.status).toBe(201)
+        expect(res.body).toMatchObject({
+            status: 'success',
+        })
+    })
+
+    test('Get all users', async () => {
+        const res = await request(app).get('/api/users')
+
+        expect(res.status).toBe(200)
+        expect(res.body).toMatchObject({
+            status: 'success',
+            users: expect.arrayContaining([expect.objectContaining({
+                email: expect.any(String)
+            })])
+        })
+    })
+
 })
